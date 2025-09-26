e40a4abb9758f91504ad367fa9eb9ac38b2cc01b
Rafa Souto <souto.rafaelb@gmail.com>
2025-09-26 23:48:36 +0000

    feat: add wallet entity

---
+++ app/Models/Wallet.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Wallet extends Model
+{
+    /** @use HasFactory<\Database\Factories\WalletFactory> */
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = ['balance'];
+}
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Document;
+use App\Models\Wallet;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,12 +16,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('document_id');
+            $table->unsignedBigInteger('wallet_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->enum('type', [UserTypeEnum::PF, UserTypeEnum::PJ])
                 ->default('PF');
             $table->foreignIdFor(Document::class);
+            $table->foreignIdFor(Wallet::class);
             $table->rememberToken();
             $table->timestamps();
         });
