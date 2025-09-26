bfaa6039b37384f8bd2c598f5e7432808c71d210
Rafa Souto <souto.rafaelb@gmail.com>
2025-09-26 23:57:28 +0000

    feat: add Transaction entity

---
+++ app/Models/Transaction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    /** @use HasFactory<\Database\Factories\TransactionFactory> */
+    use HasFactory;
+
+    protected $fillable = ['type', 'amount', 'payer_id', 'payee_id'];
+}
